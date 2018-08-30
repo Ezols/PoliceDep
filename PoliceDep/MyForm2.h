a65aa4d168261c8b204d3b8967ae2c588b7a7832
Ezols <edvards.ozols@gmail.com>
2018-08-30 22:21:42 +0300

    Combobox and ListBox

--- PoliceDep/MyForm2.h
+++ PoliceDep/MyForm2.h
@@ -19,6 +19,8 @@ namespace PoliceDep {
 		MyForm2(void)
 		{
 			InitializeComponent();
+			Fillcombo();
+			FillListBox();
 			//
 			//TODO: Add the constructor code here
 			//
@@ -44,16 +46,20 @@ namespace PoliceDep {
 	private: System::Windows::Forms::TextBox^  name_txt;
 	private: System::Windows::Forms::TextBox^  surname_txt;
 	private: System::Windows::Forms::TextBox^  age_txt;
-	private: System::Windows::Forms::ComboBox^  comboBox1;
+
+
+
 	private: System::Windows::Forms::Label^  gender_lbl;
-	private: System::Windows::Forms::ComboBox^  combobox;
-	private: System::Windows::Forms::TextBox^  combobox_txt;
 
 
 
 
-	private: System::Windows::Forms::Button^  add_btn;
+
+
+
 	private: System::Windows::Forms::Button^  delete_btn;
+	private: System::Windows::Forms::ComboBox^  searchBox;
+	private: System::Windows::Forms::ListBox^  listBox;
 
 
 
@@ -86,17 +92,15 @@ namespace PoliceDep {
 			this->name_txt = (gcnew System::Windows::Forms::TextBox());
 			this->surname_txt = (gcnew System::Windows::Forms::TextBox());
 			this->age_txt = (gcnew System::Windows::Forms::TextBox());
-			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
 			this->gender_lbl = (gcnew System::Windows::Forms::Label());
-			this->combobox = (gcnew System::Windows::Forms::ComboBox());
-			this->combobox_txt = (gcnew System::Windows::Forms::TextBox());
-			this->add_btn = (gcnew System::Windows::Forms::Button());
 			this->delete_btn = (gcnew System::Windows::Forms::Button());
+			this->searchBox = (gcnew System::Windows::Forms::ComboBox());
+			this->listBox = (gcnew System::Windows::Forms::ListBox());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(150, 217);
+			this->button1->Location = System::Drawing::Point(129, 217);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
@@ -106,7 +110,7 @@ namespace PoliceDep {
 			// 
 			// save_btn
 			// 
-			this->save_btn->Location = System::Drawing::Point(403, 78);
+			this->save_btn->Location = System::Drawing::Point(346, 217);
 			this->save_btn->Name = L"save_btn";
 			this->save_btn->Size = System::Drawing::Size(75, 23);
 			this->save_btn->TabIndex = 1;
@@ -165,52 +169,19 @@ namespace PoliceDep {
 			this->age_txt->TabIndex = 9;
 			this->age_txt->TextChanged += gcnew System::EventHandler(this, &MyForm2::textBox4_TextChanged);
 			// 
-			// comboBox1
-			// 
-			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(115, 134);
-			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(100, 21);
-			this->comboBox1->TabIndex = 10;
-			// 
 			// gender_lbl
 			// 
 			this->gender_lbl->AutoSize = true;
-			this->gender_lbl->Location = System::Drawing::Point(54, 137);
+			this->gender_lbl->Location = System::Drawing::Point(314, 61);
 			this->gender_lbl->Name = L"gender_lbl";
-			this->gender_lbl->Size = System::Drawing::Size(42, 13);
+			this->gender_lbl->Size = System::Drawing::Size(77, 13);
 			this->gender_lbl->TabIndex = 11;
-			this->gender_lbl->Text = L"Gender";
+			this->gender_lbl->Text = L"Manual search";
 			this->gender_lbl->Click += gcnew System::EventHandler(this, &MyForm2::label1_Click_1);
-			// 
-			// combobox
-			// 
-			this->combobox->FormattingEnabled = true;
-			this->combobox->Location = System::Drawing::Point(458, 172);
-			this->combobox->Name = L"combobox";
-			this->combobox->Size = System::Drawing::Size(100, 21);
-			this->combobox->TabIndex = 12;
-			// 
-			// combobox_txt
-			// 
-			this->combobox_txt->Location = System::Drawing::Point(337, 172);
-			this->combobox_txt->Name = L"combobox_txt";
-			this->combobox_txt->Size = System::Drawing::Size(100, 20);
-			this->combobox_txt->TabIndex = 13;
-			// 
-			// add_btn
-			// 
-			this->add_btn->Location = System::Drawing::Point(362, 217);
-			this->add_btn->Name = L"add_btn";
-			this->add_btn->Size = System::Drawing::Size(75, 23);
-			this->add_btn->TabIndex = 14;
-			this->add_btn->Text = L"Add";
-			this->add_btn->UseVisualStyleBackColor = true;
-			this->add_btn->Click += gcnew System::EventHandler(this, &MyForm2::add_btn_Click);
 			// 
 			// delete_btn
 			// 
-			this->delete_btn->Location = System::Drawing::Point(458, 217);
+			this->delete_btn->Location = System::Drawing::Point(466, 217);
 			this->delete_btn->Name = L"delete_btn";
 			this->delete_btn->Size = System::Drawing::Size(75, 23);
 			this->delete_btn->TabIndex = 15;
@@ -218,17 +189,33 @@ namespace PoliceDep {
 			this->delete_btn->UseVisualStyleBackColor = true;
 			this->delete_btn->Click += gcnew System::EventHandler(this, &MyForm2::delete_btn_Click);
 			// 
+			// searchBox
+			// 
+			this->searchBox->FormattingEnabled = true;
+			this->searchBox->Location = System::Drawing::Point(397, 57);
+			this->searchBox->Name = L"searchBox";
+			this->searchBox->Size = System::Drawing::Size(100, 21);
+			this->searchBox->TabIndex = 10;
+			this->searchBox->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm2::searchBox_SelectedIndexChanged);
+			// 
+			// listBox
+			// 
+			this->listBox->FormattingEnabled = true;
+			this->listBox->Location = System::Drawing::Point(317, 84);
+			this->listBox->Name = L"listBox";
+			this->listBox->Size = System::Drawing::Size(224, 108);
+			this->listBox->TabIndex = 16;
+			this->listBox->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm2::listBox_SelectedIndexChanged);
+			// 
 			// MyForm2
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(580, 261);
+			this->Controls->Add(this->listBox);
 			this->Controls->Add(this->delete_btn);
-			this->Controls->Add(this->add_btn);
-			this->Controls->Add(this->combobox_txt);
-			this->Controls->Add(this->combobox);
 			this->Controls->Add(this->gender_lbl);
-			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->searchBox);
 			this->Controls->Add(this->age_txt);
 			this->Controls->Add(this->surname_txt);
 			this->Controls->Add(this->name_txt);
@@ -246,51 +233,165 @@ namespace PoliceDep {
 #pragma endregion
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 		Application::Exit();
-		}
+	}
 	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
 	}
-private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
-}
-private: System::Void label3_Click(System::Object^  sender, System::EventArgs^  e) {
-}
-private: System::Void textBox4_TextChanged(System::Object^  sender, System::EventArgs^  e) {
-}
-private: System::Void save_btn_Click(System::Object^  sender, System::EventArgs^  e) {
+	private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void label3_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void textBox4_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void save_btn_Click(System::Object^  sender, System::EventArgs^  e) {
 
-	String^ constring = L"datasource=localhost;port=3306;username=root;password=global";
-	MySqlConnection^ conDatabase = gcnew MySqlConnection(constring);
-	MySqlCommand^ cmdDataBase = gcnew MySqlCommand("INSERT into policedep.officers (Name,Surname,Age) values('"+this->name_txt->Text+"','"+this->surname_txt->Text+"','"+this->age_txt->Text +"');", conDatabase);
-	MySqlDataReader^ myReader;
+		String^ constring = L"datasource=localhost;port=3306;username=root;password=global";
+		MySqlConnection^ conDatabase = gcnew MySqlConnection(constring);
+		MySqlCommand^ cmdDataBase = gcnew MySqlCommand("INSERT into policedep.officers (Name,Surname,Age) values('" + this->name_txt->Text + "','" + this->surname_txt->Text + "','" + this->age_txt->Text + "');", conDatabase);
+		MySqlDataReader^ myReader;
 
-	try
-	{
-		conDatabase->Open();
-		myReader = cmdDataBase->ExecuteReader();
-		MessageBox::Show("Saved");
-
-		while (myReader->Read())
+		try
 		{
+			conDatabase->Open();
+			myReader = cmdDataBase->ExecuteReader();
+			MessageBox::Show("Saved");
 
-			
+			while (myReader->Read())
+			{
+
+
+			}
+
+
+		}
+		catch (Exception^ex)
+		{
+			MessageBox::Show(ex->Message);
+		}
+	}
+	private: System::Void label1_Click_1(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void add_btn_Click(System::Object^  sender, System::EventArgs^  e) {
+
+
+	}
+	private: System::Void delete_btn_Click(System::Object^  sender, System::EventArgs^  e)
+	{
+		String^ constring = L"datasource=localhost;port=3306;username=root;password=global";
+		MySqlConnection^ conDatabase = gcnew MySqlConnection(constring);
+		MySqlCommand^ cmdDataBase = gcnew MySqlCommand("DELETE FROM policedep.officers where name = '"+this->searchBox->Text+"';", conDatabase);
+		MySqlDataReader^ myReader;
+
+		
+		try
+		{
+			conDatabase->Open();
+			myReader = cmdDataBase->ExecuteReader();
+			MessageBox::Show("Deleted");
+
+		}
+		catch (Exception^ex)
+		{
+			MessageBox::Show(ex->Message);
 		}
 
 
+
+
+
 	}
-	catch (Exception^ex)
+	private: void Fillcombo(void)
 	{
-		MessageBox::Show(ex->Message);
+		String^ constring = L"datasource=localhost;port=3306;username=root;password=global";
+		MySqlConnection^ conDatabase = gcnew MySqlConnection(constring);
+		MySqlCommand^ cmdDataBase = gcnew MySqlCommand("SELECT * FROM policedep.officers;", conDatabase);
+		MySqlDataReader^ myReader;
+
+		try
+		{
+			conDatabase->Open();
+			myReader = cmdDataBase->ExecuteReader();
+
+			while (myReader->Read())
+			{
+				String^ vName;
+				vName = myReader->GetString("Name");
+				searchBox->Items->Add(vName);		
+			}
+
+
+		}
+		catch (Exception^ex)
+		{
+			MessageBox::Show(ex->Message);
+		}
 	}
-}
-private: System::Void label1_Click_1(System::Object^  sender, System::EventArgs^  e) {
-}
-private: System::Void add_btn_Click(System::Object^  sender, System::EventArgs^  e) {
-		
-	combobox->Items->Add(combobox_txt->Text);
+			
+	private: void FillListBox(void)
+	{
+		String^ constring = L"datasource=localhost;port=3306;username=root;password=global";
+		MySqlConnection^ conDatabase = gcnew MySqlConnection(constring);
+		MySqlCommand^ cmdDataBase = gcnew MySqlCommand("SELECT * FROM policedep.officers;", conDatabase);
+		MySqlDataReader^ myReader;
 
-}
-private: System::Void delete_btn_Click(System::Object^  sender, System::EventArgs^  e) {
-		combobox->Items->Remove(combobox->Text);
+		try
+		{
+			conDatabase->Open();
+			myReader = cmdDataBase->ExecuteReader();
 
+			while (myReader->Read())
+			{
+				String^ Name;
+				Name = myReader->GetString("Name");
+				String^ Surname;
+				Surname = myReader->GetString("Surname");
+				String^ Age;
+				Age = myReader->GetInt32("Age").ToString();
+				listBox->Items->Add(Name + ", " + Surname + ", " + Age);
+
+			}
+
+
+		}
+		catch (Exception^ex)
+		{
+			MessageBox::Show(ex->Message);
+		}
+	}
+
+	private: System::Void searchBox_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+
+		String^ comboVal = searchBox->Text;
+		String^ constring = L"datasource=localhost;port=3306;username=root;password=global";
+		MySqlConnection^ conDatabase = gcnew MySqlConnection(constring);
+		MySqlCommand^ cmdDataBase = gcnew MySqlCommand("SELECT * FROM policedep.officers where name ='" + comboVal + "';", conDatabase);
+		MySqlDataReader^ myReader;
+
+		try
+		{
+			conDatabase->Open();
+			myReader = cmdDataBase->ExecuteReader();
+
+			if (myReader->Read())
+			{
+				String^ name = myReader->GetString("Name");
+				name_txt->Text = name;
+				String^ surname = myReader->GetString("Surname");
+				surname_txt->Text = surname;
+				String^ age = myReader->GetInt32("age").ToString();
+				age_txt->Text = age;
+			}
+
+
+		}
+		catch (Exception^ex)
+		{
+			MessageBox::Show(ex->Message);
+		}
+
+	}
+
+
+private: System::Void listBox_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
 }
 };
 }
